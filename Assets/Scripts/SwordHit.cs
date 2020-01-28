6ee8457345202538977c3cbda0ab18c8d952dad4
Rasmus Kyyhkynen <53016455+rkyyhkynen@users.noreply.github.com>
2020-01-28 19:28:13 +0200

    lisätty oikeat ääniraidat

---
+++ Assets/Scripts/SwordHit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordHit : MonoBehaviour
+{
+
+    public AudioClip swordHit;
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        source.PlayOneShot(swordHit);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
