eae030c3ae92ddf3ffaa90c69687841e0480a838
Rasmus Kyyhkynen <rojkyy@utu.fi>
2020-02-11 12:04:57 +0200

    Tuuli. Voidaan vaihtaa esim. tuleksi

--- Assets/Scripts/Audio/WindSound.cs
+++ Assets/Scripts/Audio/WindSound.cs
@@ -6,13 +6,12 @@ public class WindSound : MonoBehaviour
 {
     private AudioSource source;
     public AudioClip wind;
-    private float playEverySeconds = 2;
-    private float timePassed = 0;
 
     private void Awake()
     {
         source = GetComponent<AudioSource>();
         source.clip = wind;
+        source.Play();
     }
 
     // Start is called before the first frame update
@@ -24,11 +23,6 @@ public class WindSound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timePassed += Time.deltaTime;
-        if (timePassed >= playEverySeconds)
-        {
-            timePassed = 0;
-            source.Play();
-        }
+
     }
 }
